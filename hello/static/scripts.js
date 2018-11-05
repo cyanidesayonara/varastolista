374d9da2eafe513c05f4b2cf4f4f0d1c909d67a2
cyanidesayonara <cyanidesayonara@gmail.com>
2018-11-06 01:48:06 +0200

    torch-button

--- hello/static/scripts.js
+++ hello/static/scripts.js
@@ -101,35 +101,21 @@ function startQuagga() {
   });
 };
 function toggleTorch() {
-  navigator.mediaDevices.getUserMedia({
-    video: {
-      facingMode: 'environment',
+  const video = document.querySelector('video');
+  if (video) {
+    const stream = video.srcObject;
+    
+    // get the active track of the stream
+    const track = stream.getVideoTracks()[0];
+    const capabilities = track.getCapabilities();
+
+    if (capabilities.torch) {
+      track.applyConstraints({
+        advanced: [{ torch: true }]
+      })
+        .catch(e => console.log(e));
     }
-  })
-    .then((stream) => {
-      const video = document.querySelector('video');
-      video.srcObject = stream;
-
-      // get the active track of the stream
-      const track = stream.getVideoTracks()[0];
-
-      video.addEventListener('loadedmetadata', (e) => {
-        window.setTimeout(() => (
-          onCapabilitiesReady(track.getCapabilities())
-        ), 500);
-      });
-
-      function onCapabilitiesReady(capabilities) {
-        if (capabilities.torch) {
-          track.applyConstraints({
-            advanced: [{ torch: true }]
-          })
-            .catch(e => console.log(e));
-        }
-      }
-
-    })
-    .catch(err => console.error('getUserMedia() failed: ', err));
+  }
 }
 
 $(document)
