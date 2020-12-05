8f5f23c6402004df15e39cded127e0a0f0e4c2f0
Santtu Nykänen <santtu.nykanen@solidabis.com>
2020-12-05 22:18:50 +0200

    Update 2020.12
    Fix bug with search
    * before returned a queryset of all parts
    * now returns a queryset of parts owned by user, unless user is superuser
    
    Update to Django 3.1.4
    Update other packages on requirements.txt
    Update language url
    Recompile translations
    
    Add a ForeignKeyWidget to PartResource
    * owner field in excel will be represented by owner's username, not id

--- index/resources.py
+++ index/resources.py
@@ -1,6 +1,13 @@
-from import_export import resources
+from import_export import resources, fields
+from django.contrib.auth.models import User
+from import_export.widgets import ForeignKeyWidget
 from .models import Part
 
 class PartResource(resources.ModelResource):
+    owner = fields.Field(
+        column_name='owner',
+        attribute='owner',
+        widget=ForeignKeyWidget(User, 'username'))
+
     class Meta:
         model = Part
